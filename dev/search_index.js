3d90340e34fb2db70f43347cbe544a2148b38375
Documenter.jl <documenter@juliadocs.github.io>
2022-05-20 23:28:07 +0000

    build based on 2a41501

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MaxPro","category":"page"},{"location":"#MaxPro","page":"Home","title":"MaxPro","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MaxPro.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MaxPro]","category":"page"}]
+}
