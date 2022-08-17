245675574b891be49537e23ffeff8e4586adb4f6
ArnoStrouwen <arno.strouwen@telenet.be>
2022-08-17 03:44:49 +0200

    Basic functionality

--- src/MaxPro.jl
+++ src/MaxPro.jl
@@ -1,11 +1,35 @@
 module MaxPro
 
-function sample(n::Int,lb::AbstractVector{T},ub::AbstractVector{T}) where T
-    design = lb
-    for i in 2:n
-        design = hcat(design,lb)
-    end
-    return design
-end
+using LatinHypercubeSampling
+using Optimization, OptimizationOptimJL
+using ReverseDiff
+using Random
 
+function max_pro_criterion(design::AbstractMatrix{T}) where T
+    n,d = size(design)
+    sum_d = zero(T)
+    for i in 1:n-1, j in i+1:n
+        pointi = @view design[i,:]
+        pointj = @view design[j,:]
+        sum_d += inv(prod((pointi .- pointj).^2))
+    end
+    return (sum_d/T(binomial(n,2)))^T(1/d)
+end
+max_pro_criterion(design,p::SciMLBase.NullParameters) = max_pro_criterion(design)
+
+function max_pro_design(design0::AbstractMatrix{T}) where T
+    n,d = size(design0)
+    f = OptimizationFunction(max_pro_criterion, Optimization.AutoReverseDiff())
+    opti_prob = OptimizationProblem(f, design0, p=nothing, lb = zeros(n,d), ub = ones(n,d))
+    opti_sol = solve(opti_prob, Fminbox(BFGS())) #TBD: make BFGS options available to user
+    return opti_sol.u
+end
+function max_pro_design(n::Int,d::Int)
+    n>zero(n) || throw(DomainError(n,"Number of design points n must be strictly positive"))
+    d>zero(d) || throw(DomainError(d,"Dimensions d must be strictly positive"))
+    design0, _ = LHCoptim(n,d,1000;rng=MersenneTwister(0)) #TBD: make LHC options available to user
+    design0 = (design0.-one(n))./(n-one(n))
+    max_pro_design(design0)
+end
+export max_pro_design
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
 using MaxPro
 using Test
+using Random
 
 @testset "MaxPro.jl" begin
-    @test MaxPro.sample(2,[0.0,0.1],[1.0,2.0]) == [0.0 0.0; 0.1 0.1]
+    n,d = 25,8
+    design0, _ = MaxPro.LHCoptim(n,d,1000;rng=MersenneTwister(0))
+    design0 = (design0.-one(n))./(n-one(n))
+    @test MaxPro.max_pro_criterion(design0) ≈ 30.4333599696552
+    @test MaxPro.max_pro_criterion(max_pro_design(design0)) ≈ 19.7820616759956
+    @test MaxPro.max_pro_criterion(max_pro_design(n,d)) ≈ 19.7820616759956
+    @test_throws DomainError max_pro_design(-1,1)
+    @test_throws DomainError max_pro_design(1,-1)
 end
