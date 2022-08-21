681f61523913a243c2fdd310e68d171d5c1abd36
ArnoStrouwen <arno.strouwen@telenet.be>
2022-08-22 00:13:53 +0200

    apply styleguide

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,21 @@
 using MaxPro
 using Documenter
 
-DocMeta.setdocmeta!(MaxPro, :DocTestSetup, :(using MaxPro); recursive=true)
+DocMeta.setdocmeta!(MaxPro, :DocTestSetup, :(using MaxPro); recursive = true)
 
 makedocs(;
-    modules=[MaxPro],
-    authors="ArnoStrouwen <arno.strouwen@telenet.be> and contributors",
-    repo="https://github.com/ArnoStrouwen/MaxPro.jl/blob/{commit}{path}#{line}",
-    sitename="MaxPro.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://ArnoStrouwen.github.io/MaxPro.jl",
-        assets=String[],
-    ),
-    pages=[
-        "Overview" => "index.md",
-    ],
-)
+         modules = [MaxPro],
+         authors = "ArnoStrouwen <arno.strouwen@telenet.be> and contributors",
+         repo = "https://github.com/ArnoStrouwen/MaxPro.jl/blob/{commit}{path}#{line}",
+         sitename = "MaxPro.jl",
+         format = Documenter.HTML(;
+                                  prettyurls = get(ENV, "CI", "false") == "true",
+                                  canonical = "https://ArnoStrouwen.github.io/MaxPro.jl",
+                                  assets = String[]),
+         pages = [
+             "Overview" => "index.md",
+         ])
 
 deploydocs(;
-    repo="github.com/ArnoStrouwen/MaxPro.jl",
-    devbranch="master",
-)
+           repo = "github.com/ArnoStrouwen/MaxPro.jl",
+           devbranch = "master")
