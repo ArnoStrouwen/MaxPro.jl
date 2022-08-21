79a20608345dc5509236578ed92596dec1071e27
ArnoStrouwen <arno.strouwen@telenet.be>
2022-08-21 14:14:06 +0200

    switch optim to nlopt

--- src/MaxPro.jl
+++ src/MaxPro.jl
@@ -1,10 +1,15 @@
 module MaxPro
 
 using LatinHypercubeSampling
-using Optimization, OptimizationOptimJL
+using Optimization, OptimizationNLopt
 using ReverseDiff
 using Random
 
+function max_pro_criterion(design::AbstractArray{T},p::NamedTuple{(:n, :d), Tuple{V, V}}) where{T, V<:Integer}
+    n,d = p.n, p.d
+    design = reshape(design,n,d)
+    return max_pro_criterion(design)
+end
 function max_pro_criterion(design::AbstractMatrix{T}) where T
     n,d = size(design)
     sum_d = zero(T)
@@ -15,14 +20,13 @@ function max_pro_criterion(design::AbstractMatrix{T}) where T
     end
     return (sum_d/T(binomial(n,2)))^T(1/d)
 end
-max_pro_criterion(design,p::SciMLBase.NullParameters) = max_pro_criterion(design)
-
-function max_pro_design(design0::AbstractMatrix{T}) where T
+function max_pro_design(design0::Matrix{T}) where T
     n,d = size(design0)
+    design0 = reshape(design0,n*d)
     f = OptimizationFunction(max_pro_criterion, Optimization.AutoReverseDiff())
-    opti_prob = OptimizationProblem(f, design0, p=nothing, lb = zeros(n,d), ub = ones(n,d))
-    opti_sol = solve(opti_prob, Fminbox(BFGS())) #TBD: make BFGS options available to user
-    return opti_sol.u
+    opti_prob = OptimizationProblem(f, design0, (n=n,d=d), lb = zeros(n*d), ub = ones(n*d))
+    opti_sol = solve(opti_prob, NLopt.LD_LBFGS()) #TBD: make BFGS options available to user
+    return reshape(opti_sol.u,n,d)
 end
 function max_pro_design(n::Int,d::Int)
     n>zero(n) || throw(DomainError(n,"Number of design points n must be strictly positive"))
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,39 @@ using Test
 using Random
 
 @testset "MaxPro.jl" begin
-    n,d = 25,8
+    _design0 =[
+                23 2 7 9 18 7 14 3;
+                16 24 14 5 9 20 2 21;
+                12 10 5 13 17 25 7 1;
+                5 23 17 14 12 10 1 2;
+                9 17 3 18 23 14 22 15;
+                13 20 21 3 25 9 9 12;
+                8 8 9 17 5 1 23 4;
+                25 21 16 10 4 23 17 8;
+                7 1 11 2 20 3 11 19;
+                20 6 12 22 13 24 24 18;
+                21 9 15 21 19 5 5 5;
+                11 19 2 1 14 4 8 16;
+                19 11 25 20 1 15 16 7;
+                22 18 1 24 8 11 10 9;
+                1 5 19 25 10 8 15 17;
+                24 14 20 12 22 19 20 22;
+                10 15 8 23 15 13 3 25;
+                4 22 6 19 2 18 13 13;
+                6 13 24 16 24 22 21 10;
+                17 4 4 6 11 17 12 24;
+                18 3 22 11 6 12 4 20;
+                3 12 10 4 3 16 18 6;
+                2 7 23 8 16 21 6 14;
+                15 25 18 15 7 6 19 23;
+                14 16 13 7 21 2 25 11]
+    n,d = size(_design0)
     design0, _ = MaxPro.LHCoptim(n,d,1000;rng=MersenneTwister(0))
+    @test design0 == _design0
     design0 = (design0.-one(n))./(n-one(n))
-    @test isapprox(MaxPro.max_pro_criterion(design0), 30.43,atol=0.01)
-    @test isapprox(MaxPro.max_pro_criterion(max_pro_design(design0)), 19.78,atol=0.01)
-    @test isapprox(MaxPro.max_pro_criterion(max_pro_design(n,d)), 19.78,atol=0.01)
+    @test isapprox(MaxPro.max_pro_criterion(design0), 30.4333599696552,atol=1e-7)
+    @test isapprox(MaxPro.max_pro_criterion(max_pro_design(design0)), 19.26809557894501,atol=1e-7)
+    @test isapprox(MaxPro.max_pro_criterion(max_pro_design(n,d)), 19.26809557894501,atol=1e-7)
     @test_throws DomainError max_pro_design(-1,1)
     @test_throws DomainError max_pro_design(1,-1)
 end
